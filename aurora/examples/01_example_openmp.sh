be6d25dcd8bf6b3fab826f819894ab24676b18ce
Chris Knight <knight@aurora-uan-0010.hostmgmt.cm.aurora.alcf.anl.gov>
2025-02-03 05:15:23 +0000

    update scripts

--- aurora/examples/01_example_openmp.sh
+++ aurora/examples/01_example_openmp.sh
@@ -1,30 +1,13 @@
 #!/bin/bash -l
 #PBS -l select=1
 #PBS -l place=scatter
-#PBS -l walltime=0:15:00
+#PBS -l walltime=0:10:00
 #PBS -q debug
-#PBS -A Catalyst
+#PBS -A <project-name>
 #PBS -l filesystems=home:flare
+#PBS -o logs/
+#PBS -e logs/
 
 cd ${PBS_O_WORKDIR}
 
-# MPI example w/ 4 MPI ranks per node spread evenly across cores
-NNODES=`wc -l < $PBS_NODEFILE`
-NRANKS_PER_NODE=12
-NDEPTH=1
-NTHREADS=1
-
-NTOTRANKS=$(( NNODES * NRANKS_PER_NODE ))
-echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS_PER_NODE} THREADS_PER_RANK= ${NTHREADS}"
-
-EXE=./vecadd
-
-MPI_ARG="-n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth "
-
-# select subdevices by default or use helper script to affinitize MPI ranks to tiles
-#export LIBOMPTARGET_DEVICES=subdevice
-EXE="../../../../HelperScripts/Aurora/set_affinity_gpu.sh ${EXE}"
-
-COMMAND="mpiexec ${MPI_ARG} ${EXE}"
-echo "COMMAND= ${COMMAND}"
-${COMMAND}
+mpiexec -n 1 --ppn 1 ./01_example_openmp
